0e3f8849e7b0f893a5eb3c9af4f8899ee8e80884
payflow02-coder <payflow02@gmail.com>
2026-01-11 22:04:21 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function generateCheck() {
-  const id = "PF-" + Math.floor(100000 + Math.random() * 900000);
-  const date = new Date().toLocaleString("kk-KZ");
+  const randomId = "PF-" + Math.floor(100000 + Math.random() * 900000);
+  document.getElementById("checkId").innerText = randomId;
 
-  document.getElementById("checkId").innerText = id;
-  document.getElementById("date").innerText = date;
+  const now = new Date();
+  document.getElementById("date").innerText =
+    now.toLocaleDateString("kk-KZ") + ", " +
+    now.toLocaleTimeString("kk-KZ");
 }
