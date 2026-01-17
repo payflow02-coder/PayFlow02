8ec3047f241d51666b2accf8dc6c6c140e9a7aae
payflow02-coder <payflow02@gmail.com>
2026-01-17 15:57:35 +0600

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function onlyNumbers(input) {
   input.value = input.value.replace(/[^0-9]/g, "");
 }
 
-// Төлем логотипін ауыстыру
+// Төлем логотипі
 function updateLogo() {
   const payment = document.getElementById("payment").value;
   const logo = document.getElementById("payLogo");
@@ -23,13 +23,14 @@ function updateLogo() {
 }
 
 function generate() {
-  const name = document.getElementById("name").value.trim();
+  const seller = document.getElementById("seller").value.trim();
+  const buyer = document.getElementById("buyer").value.trim();
   const phone = document.getElementById("phone").value.trim();
   const item = document.getElementById("item").value.trim();
   const amount = document.getElementById("amount").value.trim();
   const payment = document.getElementById("payment").value;
 
-  if (!name || !phone || !item || !amount || !payment) {
+  if (!seller || !buyer || !phone || !item || !amount || !payment) {
     alert("Барлық өрісті толық толтырыңыз!");
     return;
   }
@@ -44,7 +45,7 @@ function generate() {
   qrBox.innerHTML = "";
 
   new QRCode(qrBox, {
-    text: `PayFlow чек | ${name} | ${amount}₸ | ${payment}`,
+    text: `PayFlow чек | Сатушы: ${seller} | Сатып алушы: ${buyer} | ${amount}₸ | ${payment}`,
     width: 128,
     height: 128
   });
@@ -54,11 +55,12 @@ function generate() {
   const pdf = new jsPDF();
 
   pdf.text("PayFlow Digital Check", 20, 20);
-  pdf.text(`Аты: ${name}`, 20, 40);
-  pdf.text(`Телефон: ${phone}`, 20, 50);
-  pdf.text(`Тауар: ${item}`, 20, 60);
-  pdf.text(`Сома: ${amount} ₸`, 20, 70);
-  pdf.text(`Төлем: ${payment}`, 20, 80);
+  pdf.text(`Сатушы: ${seller}`, 20, 40);
+  pdf.text(`Сатып алушы: ${buyer}`, 20, 50);
+  pdf.text(`Телефон: ${phone}`, 20, 60);
+  pdf.text(`Тауар: ${item}`, 20, 70);
+  pdf.text(`Сома: ${amount} ₸`, 20, 80);
+  pdf.text(`Төлем: ${payment}`, 20, 90);
 
   pdf.save("payflow-check.pdf");
 }
